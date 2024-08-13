d9e1ff75656cfbd12eaadf5163b9b5905e23eb05
Oknown15 <buchijunior90@gmail.com>
2024-08-13 23:34:43 +0100

    prods

--- client/src/views/admin/login.jsx
+++ client/src/views/admin/login.jsx
@@ -47,7 +47,7 @@ function Login() {
 
     return (
         <div>
-            <section className="bg-gray-50 dark:bg-gray-900">
+            <section className="bg-gray-900">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
                     <div className="flex flex-col justify-center">
                         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to Laptop Haven</h1>
