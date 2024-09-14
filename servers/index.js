a874ed5b4d0660ed2c9579153722671fed45e358
Oknown15 <buchijunior90@gmail.com>
2024-09-14 01:04:02 +0100

    prod err

--- servers/index.js
+++ servers/index.js
@@ -38,6 +38,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
+app.use(cors({
+    origin: 'https://laptop-ecom.vercel.app' 
+}));
 
 
 const corsOptions = {
