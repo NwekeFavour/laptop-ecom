ed5336322e72f5b2c373680cc0e6d4fb416e9f3d
Oknown15 <buchijunior90@gmail.com>
2024-09-17 21:10:50 +0100

    prod err

--- client/src/views/admin/register.jsx
+++ client/src/views/admin/register.jsx
@@ -19,7 +19,7 @@ function Register() {
     }
 
     try {
-        const response = await fetch('https://laptop-ecom-yfz7.vercel.app/register', {
+        const response = await fetch('https://laptop-ecom-yfz7.vercel.app/api/register', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
