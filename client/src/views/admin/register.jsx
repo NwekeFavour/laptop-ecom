60826835338cb0720ae211c892761b83167e6d71
Oknown15 <buchijunior90@gmail.com>
2024-09-17 16:47:18 +0100

    prod err

--- client/src/views/admin/register.jsx
+++ client/src/views/admin/register.jsx
@@ -19,7 +19,7 @@ function Register() {
     }
 
     try {
-        const response = await fetch('/register', {
+        const response = await fetch('http://laptop-ecom-dsd2.vercel.app/register', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
